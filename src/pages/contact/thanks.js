112e5bb4071d6b915ce50b27570355d486c2d9dd
finewitch <finewitch@gmail.com>
2019-09-09 18:18:46 +0300

    redirect from contact form

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import ArrowIcon from '../../components/atoms/ArrowIcon'
+import { graphql, Link } from 'gatsby'
 
 export default class Thanks extends React.Component {
   render(){
@@ -8,10 +10,14 @@ export default class Thanks extends React.Component {
   <Layout>
     
     <section className="section">
-      <div className="container section__wrapper">
+      <div className="container thanks section__wrapper">
+        <Link to="/#contact" state={{ location: 'contact'}} className="goback back-arrow">      
+              <ArrowIcon/>
+        </Link>
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h2>Your message will be delivered soon</h2>
+          <h4>In 3 seconds you'll be redirected back to main page</h4>
         </div>
       </div>
     </section>
@@ -20,5 +26,8 @@ export default class Thanks extends React.Component {
 )}
   componentDidMount(){
     console.log('redirection')
+    setTimeout(()=>{
+      window.location = '/';
+    }, 3000)
   }
   }
