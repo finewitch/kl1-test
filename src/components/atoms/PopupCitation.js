717711e8e2b2e76159dd96a3ca25487ce90731a1
finewitch <finewitch@gmail.com>
2020-01-19 22:42:27 +0000

    resources additional field

--- src/components/atoms/PopupCitation.js
+++ src/components/atoms/PopupCitation.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const PopupCitation = ({data, onClickClose}) => {
-    // console.log(data, 'in ppup')
     let APA = Object.keys(data)[0];
     let Vancouver = Object.keys(data)[2];
     let Harvard = Object.keys(data)[1];
@@ -10,7 +9,6 @@ export const PopupCitation = ({data, onClickClose}) => {
 
     function generateRow(type){
 
-        // console.log('type--  ', type)
         return (
             <tr>
                 <td className="name">{type.toUpperCase()}</td>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -33,54 +33,54 @@ export class IndexPageTemplate extends React.Component {
     // this.inViewPort = false;
     this.updateStateWithPageLocation = function(){
 
-      // const menu = document.querySelector('.menu');
-      // const menuLinks = document.querySelectorAll('.menu-links');
-      // const secondSec = document.querySelector('.about')
+      const menu = document.querySelector('.menu');
+      const menuLinks = document.querySelectorAll('.menu-links');
+      const secondSec = document.querySelector('.about')
   
       var section = document.querySelectorAll(".hidden");
       var sections = {};
-      // var i = 0;
+      var i = 0;
     
       Array.prototype.forEach.call(section, function(e) {
         sections[e.id] = e.offsetTop;
       });
 
 
-        // window.onscroll = function() {
+        window.onscroll = function() {
 
-        //   var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+          var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
 
-        //   menuLinks.forEach(el=>{
-        //     el.classList.remove('active');
-        //   })
+          menuLinks.forEach(el=>{
+            el.classList.remove('active');
+          })
       
-        //   for (i in sections) {
-        //     if (scrollPosition  >= sections['about']/1.2){
-        //       // console.log('here we go');
-        //       secondSec.querySelectorAll('.n-visible').forEach(el=>{
-        //         el.classList.add('is-visible')
-        //       })
-        //       secondSec.querySelectorAll('.border-t-none').forEach(el=>{
-        //         el.classList.add('border-t-visible')
-        //       })
-        //       secondSec.querySelectorAll('.border-b-none').forEach(el=>{
-        //         el.classList.add('border-b-visible')
-        //       })
+          for (i in sections) {
+            if (scrollPosition  >= sections['about']/1.2){
+              // console.log('here we go');
+              secondSec.querySelectorAll('.n-visible').forEach(el=>{
+                el.classList.add('is-visible')
+              })
+              secondSec.querySelectorAll('.border-t-none').forEach(el=>{
+                el.classList.add('border-t-visible')
+              })
+              secondSec.querySelectorAll('.border-b-none').forEach(el=>{
+                el.classList.add('border-b-visible')
+              })
 
-        //     }
+            }
 
-        //     // MAKE CHANGE IN PUBLICATIONS COMPONENT PROPS TO TRIGER THE TYPEIN PLUGIN
-        //     if (scrollPosition  >= ( sections['team']* 1.2) ){
-        //       window.myTypeItInstance.go()
-        //     }
+            // MAKE CHANGE IN PUBLICATIONS COMPONENT PROPS TO TRIGER THE TYPEIN PLUGIN
+            if (scrollPosition  >= ( sections['team']* 1.2) ){
+              window.myTypeItInstance.go()
+            }
 
-        //     if (sections[i] <= scrollPosition) {
+            if (sections[i] <= scrollPosition) {
 
-        //       menu.querySelector('.active').classList.remove('active')
-        //       menu.querySelector('.menu-' + i ).classList.add('active');
+              menu.querySelector('.active').classList.remove('active')
+              menu.querySelector('.menu-' + i ).classList.add('active');
 
-        //     }
-        //     }}
+            }
+            }}
           }
   }
   render(){
