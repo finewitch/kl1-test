942503b14f59a1f74078f94e0b7276ec5d97805b
finewitch <finewitch@gmail.com>
2020-02-02 19:33:16 +0000

    mobile ready main site

--- src/components/News.js
+++ src/components/News.js
@@ -23,24 +23,7 @@ export default class News extends React.Component {
             prevArrow: <SamplePrevArrow />,
             responsive: [
                 {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                  }
-                },
-                {
                   breakpoint: 600,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 1
-                  }
-                },
-                {
-                  breakpoint: 480,
                   settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
