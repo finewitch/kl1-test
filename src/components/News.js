223489067e1929b42c656f2dfb8ca359bd912691
finewitch <finewitch@gmail.com>
2020-02-02 18:57:10 +0000

    publications mobile

--- src/components/News.js
+++ src/components/News.js
@@ -21,6 +21,32 @@ export default class News extends React.Component {
             slidesToScroll: 1,
             nextArrow: <SampleNextArrow />,
             prevArrow: <SamplePrevArrow />,
+            responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ],
             customPaging: function(i) {
                 return <span className="counter">{i + 1} of {props.data.length}</span>;
               },
